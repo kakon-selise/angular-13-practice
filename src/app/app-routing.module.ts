ca8f2157028428547a99135a81cd9e18c09d441d
kakon-selise <kakon.mehedi@selise.ch>
2023-03-25 22:31:07 +0600

    Data table and home page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppDataTableComponent } from './app-data-table/app-data-table.component';
+import { AppHomePageComponent } from './app-home-page/app-home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		component: AppHomePageComponent,
+		pathMatch: 'full',
+	},
+	{
+		path: 'home',
+		component: AppHomePageComponent,
+		pathMatch: 'full',
+	},
+	{
+		path: 'data-table',
+		component: AppDataTableComponent,
+	},
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
