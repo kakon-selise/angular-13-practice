dde17a21e83fae56d7f7efaaf627ec3b499852ba
kakon-selise <kakon.mehedi@selise.ch>
2023-03-26 12:14:38 +0600

    Data table added

--- src/app/app-data-table/app-data-table.component.ts
+++ src/app/app-data-table/app-data-table.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface PeriodicElement {
 	name: string;
@@ -25,11 +28,27 @@ const ELEMENT_DATA: PeriodicElement[] = [
 	templateUrl: './app-data-table.component.html',
 	styleUrls: ['./app-data-table.component.scss'],
 })
-export class AppDataTableComponent implements OnInit {
+export class AppDataTableComponent implements OnInit, AfterViewInit {
+	displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+	dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+	@ViewChild(MatSort) sort: MatSort;
+	@ViewChild(MatPaginator) paginator: MatPaginator;
+
 	constructor() {}
 
-	displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-	dataSource = ELEMENT_DATA;
+	getRowData(row: PeriodicElement) {
+		console.log(row);
+	}
+
+	applyFilter(filterValue: string) {
+		this.dataSource.filter = filterValue.trim().toLocaleLowerCase();
+	}
 
 	ngOnInit(): void {}
+
+	ngAfterViewInit(): void {
+		this.dataSource.sort = this.sort;
+		this.dataSource.paginator = this.paginator;
+	}
 }
