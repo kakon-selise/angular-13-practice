40965c33e5a1113b5a896fd0916034f135ba86f0
kakon-selise <kakon.mehedi@selise.ch>
2023-04-02 16:32:38 +0600

    Question and option is Implimented by MathQuill text editor

--- src/app/app-quiz/components/question/question.component.ts
+++ src/app/app-quiz/components/question/question.component.ts
@@ -7,6 +7,9 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 	styleUrls: ['./question.component.scss'],
 })
 export class QuestionComponent implements OnInit {
+	pin: boolean = false;
+	pinColor = 'grey';
+
 	editorConfig = {
 		toolbar: [
 			['bold', 'italic', 'underline', 'strike'], // toggled buttons
@@ -15,10 +18,8 @@ export class QuestionComponent implements OnInit {
 			[{ header: 1 }, { header: 2 }], // custom button values
 			[{ list: 'ordered' }, { list: 'bullet' }],
 			[{ script: 'sub' }, { script: 'super' }], // superscript/subscript
-			[{ indent: '-1' }, { indent: '+1' }], // outdent/indent
-			[{ direction: 'rtl' }], // text direction
 
-			[{ size: ['small', false, 'large', 'huge'] }], // custom dropdown
+			
 			[{ header: [1, 2, 3, 4, 5, 6, false] }],
 
 			[{ color: [] }, { background: [] }], // dropdown with defaults from theme
@@ -33,23 +34,41 @@ export class QuestionComponent implements OnInit {
 
 	constructor(private fb: FormBuilder) {}
 
+	togglePin() {
+		this.pin = !this.pin;
+
+		if (this.pin) {
+			this.pinColor = 'green';
+		}
+
+		if (!this.pin) {
+			this.pinColor = 'grey';
+			const elements = document.querySelectorAll('.kkn-show-block');
+
+			for (let i = 0; i < elements.length; i++) {
+				elements[i].classList.add('kkn-show-none');
+				elements[i].classList.remove('kkn-show-block');
+			}
+		}
+	}
+
 	onSelectionChanged = (event: any) => {
 		if (event.oldRange == null) {
 			this.onFocus(event);
 		}
-		if (event.range == null) {
+		if (event.range == null && !this.pin) {
 			this.onBlur(event);
 		}
 	};
 
 	onFocus(event: any) {
-		event.editor.theme.modules.toolbar.container.classList.remove('show-none');
-		event.editor.theme.modules.toolbar.container.classList.add('show-block');
+		event.editor.theme.modules.toolbar.container.classList.remove('kkn-show-none');
+		event.editor.theme.modules.toolbar.container.classList.add('kkn-show-block');
 	}
 
 	onBlur(event: any) {
-		event.editor.theme.modules.toolbar.container.classList.add('show-none')
-		event.editor.theme.modules.toolbar.container.classList.remove('show-block')
+		event.editor.theme.modules.toolbar.container.classList.add('kkn-show-none');
+		event.editor.theme.modules.toolbar.container.classList.remove('kkn-show-block');
 	}
 
 	addQuestionForm: FormGroup = this.fb.group({
