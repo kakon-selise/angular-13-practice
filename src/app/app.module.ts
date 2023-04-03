001e25c129688fd1c38fad92f8fe89ff1d458159
kakon-selise <kakon.mehedi@selise.ch>
2023-04-03 09:55:56 +0600

    Adding flex layout library to the dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { LayoutModule } from '@angular/cdk/layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -11,14 +12,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ArchivedComponent } from './app-cooking/components/archived/archived.component';
 import { CreateRecipeComponent } from './app-cooking/components/create-recipe/create-recipe.component';
+import { AppDataTableComponent } from './app-data-table/app-data-table.component';
+import { AppHomePageComponent } from './app-home-page/app-home-page.component';
 import { FooterComponent } from './app-layouts/footer/footer.component';
 import { NavigationComponent } from './app-layouts/navigation/navigation.component';
 import { StartGameComponent } from './app-playing/components/start-game/start-game.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './shared/material/material.module';
-import { AppDataTableComponent } from './app-data-table/app-data-table.component';
-import { AppHomePageComponent } from './app-home-page/app-home-page.component';
 
 @NgModule({
 	declarations: [
@@ -44,6 +45,7 @@ import { AppHomePageComponent } from './app-home-page/app-home-page.component';
 		MatSidenavModule,
 		MatIconModule,
 		MatListModule,
+		FlexLayoutModule
 	],
 	providers: [],
 	bootstrap: [AppComponent],
