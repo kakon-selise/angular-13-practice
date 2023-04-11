d8cfb4a5e34e5ceb1429619fdc7f4d4523164b3f
kakon-selise <kakon.mehedi@selise.ch>
2023-04-11 15:50:43 +0600

    new

---
+++ src/app/app-mathquill/components/mathquill-keyboard/mathquill-keyboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mathquill-keyboard',
+  templateUrl: './mathquill-keyboard.component.html',
+  styleUrls: ['./mathquill-keyboard.component.scss']
+})
+export class MathquillKeyboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
