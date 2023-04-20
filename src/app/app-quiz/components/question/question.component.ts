d1d8e5d8324679f1f5336b76ba1a374159190b3c
kakon-selise <kakon.mehedi@selise.ch>
2023-04-20 16:28:37 +0600

    Basic post question implemented

--- src/app/app-quiz/components/question/question.component.ts
+++ src/app/app-quiz/components/question/question.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { QuizService } from '../../services/quiz.service';
 
 // Load WIRISplugins.js dynamically
@@ -14,35 +15,26 @@ document.head.appendChild(jsDemoImagesTransform);
 	styleUrls: ['./question.component.scss'],
 })
 export class QuestionComponent implements OnInit {
-	pin: boolean = false;
+	saveQuestionDisable: boolean = false;
 	pinColor = 'grey';
 	myQuestions: any[];
+	isLoading: boolean = false;
+	error: boolean = false;
+	errorMsg: string = '';
+	success: boolean = false;
+	successMsg: string = 'Data uploaded successfully';
 
-	constructor(private fb: FormBuilder, private quizService: QuizService) {}
+	constructor(private fb: FormBuilder, private quizService: QuizService, private router: Router) {}
 
-	togglePin() {
-		this.pin = !this.pin;
-
-		if (this.pin) {
-			this.pinColor = 'green';
-		}
-
-		if (!this.pin) {
-			this.pinColor = 'grey';
-			const elements = document.querySelectorAll('.kkn-show-block');
-
-			for (let i = 0; i < elements.length; i++) {
-				elements[i].classList.add('kkn-show-none');
-				elements[i].classList.remove('kkn-show-block');
-			}
-		}
+	onFormValueChange() {
+		alert('Done');
 	}
 
 	addQuestionForm: FormGroup = this.fb.group({
 		questions: this.fb.array([
 			this.fb.group({
 				question: ['', Validators.required],
-				image: ['', Validators.required],
+				image: ['imgUrl'],
 				options: this.fb.array([
 					this.fb.group({
 						option: ['', [Validators.required]],
@@ -80,10 +72,10 @@ export class QuestionComponent implements OnInit {
 	}
 
 	onAddNewQuestion() {
-
+		this.saveQuestionDisable = false;
 		const newQuestion = this.fb.group({
 			question: ['', Validators.required],
-			image: ['', Validators.required],
+			image: ['imageUrl'],
 			options: this.fb.array([
 				this.fb.group({
 					option: ['', [Validators.required]],
@@ -113,25 +105,42 @@ export class QuestionComponent implements OnInit {
 		this.QuestionsArray.push(newQuestion);
 	}
 
-	onSaveQuestions(){
-		let formData = this.addQuestionForm.getRawValue(); 
-		formData.questions.forEach((questionData: any)=>{
-			this.quizService.postQuestion(questionData).subscribe((data)=>{
-				this.getMyQuestions();
-			})
+	onSaveQuestions() {
+		this.isLoading = true;
+		let formData = this.addQuestionForm.getRawValue();
+		formData.questions.forEach((questionData: any) => {
+			this.quizService.postQuestion(questionData).subscribe({
+				next: (data) => {},
+
+				error: (err) => {
+					this.error = true;
+					this.isLoading = false;
+					this.errorMsg = err.statusText;
+				},
+
+				complete: () => {
+					this.isLoading = false;
+					this.error = false;
+					this.success = true;
+					setTimeout(() => {
+						this.getMyQuestions();
+						this.addQuestionForm.reset();
+						this.router.navigateByUrl('/quiz', { replaceUrl: true }).then(() => {
+							window.location.reload();
+						});
+					}, 2000);
+				},
+			});
 		});
-
 	}
 
-	getMyQuestions(){
-		this.quizService.getMyQuestions().subscribe((data:any)=>{
+	getMyQuestions() {
+		this.quizService.getMyQuestions().subscribe((data: any) => {
 			this.myQuestions = data;
-		})
+		});
 	}
 
-	onAddQuestionSubmit() {
-		
-	}
+	onAddQuestionSubmit() {}
 
 	ngOnInit(): void {}
 
@@ -142,24 +151,28 @@ export class QuestionComponent implements OnInit {
 	public content: string =
 		'<p class="text"> Double click on the following formula to edit it.</p><p style="text-align: center;"><math><mi>z</mi><mo>=</mo><mfrac><mrow><mo>-</mo><mi>b</mi><mo>&PlusMinus;</mo><msqrt><msup><mi>b</mi><mn>3</mn></msup><mo>-</mo><mn>4</mn><mi>a</mi><mi>c</mi></msqrt></mrow><mrow><mn>2</mn><mi>a</mi></mrow></mfrac></math></p>';
 
-	// Set options for the editor.
 	public options: Object = {
-		// Define the toolbar options for the froala editor.
 		toolbarButtons: ['undo', 'redo', 'bold', 'italic', '|', 'wirisEditor', 'wirisChemistry', 'insertImage'],
-		// Add [MW] uttons to the image editing popup Toolbar.
 		imageEditButtons: ['wirisEditor', 'wirisChemistry', 'imageDisplay', 'imageAlign', 'imageInfo', 'imageRemove'],
-		// Allow all the tags to understand the mathml
+
 		htmlAllowedTags: ['.*'],
 		htmlAllowedAttrs: ['.*'],
-		// List of tags that are not removed when they have no content inside
-		// so that formulas renderize propertly
+
+		// for remove p tag by default
+		//  It's an enum, you can specify the values numerically. The following assignment works for me:
+		// 0 = ENTER_P
+		// 1 = ENTER_DIV
+		// 2 = ENTER_BR
+
+		enter: 2,
+
 		htmlAllowedEmptyTags: ['mprescripts', 'none'],
-		// In case you are using a different Froala editor language than default,
-		// language: 'es',
-		// You can choose the language for the MathType editor, too:
-		// @see: https://docs.wiris.com/en/mathtype/mathtype_web/sdk-api/parameters#regional_properties
-		// mathTypeParameters: {
-		//   editorParameters: { language: 'es' },
-		// },
+
+		paragraphFormat: '',
+		toolbarSticky: false,
 	};
+
+	onCancelIcon(){
+		this.error = false;
+	}
 }
